ab7585ad91b35841922dd296322ac8428c9e532a
Beramme <bramraysky232@gmail.com>
2023-06-22 20:01:29 +0700

    finish about u page

--- Ticketing/storage/framework/views/9145e237bc79f58c6271188a6c079010.php
+++ Ticketing/storage/framework/views/9145e237bc79f58c6271188a6c079010.php
@@ -1,4 +1,88 @@
 <?php $__env->startSection('container'); ?>
-<h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit odio non metus viverra, id pharetra mauris semper. Maecenas id risus sagittis, aliquet dui sed, ultricies arcu. Nullam sed libero non nisl placerat gravida. Nunc vitae ante euismod, lacinia elit ut, semper nunc. In ac gravida lorem. Sed dapibus tellus at mauris consectetur, eu consequat risus volutpat. Nulla facilisi. Curabitur et erat vitae turpis euismod posuere. Maecenas eleifend est ut est fringilla, vitae dignissim mi efficitur. Duis sagittis justo id quam tristique fermentum. Donec tristique metus in enim cursus, at auctor nulla semper. Cras dapibus elit eget enim fringilla consectetur. Suspendisse potenti. Nunc vel feugiat mauris, at volutpat ligula. Suspendisse potenti. Vivamus eu fringilla felis. Nam sit amet varius eros, eu aliquam enim.</h1>
+    <link rel="stylesheet" href="/css/about.css">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-8 col-lg-6" style="">
+                <!-- Section Heading-->
+                <div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s"
+                    style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                    <h3>Our Creative <span> Team</span></h3>
+                    <p>For Developing TicketVerse</p>
+                    <div class="line"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <!-- Single Advisor-->
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s"
+                    style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                    <!-- Team Thumb-->
+                    <div class="advisor_thumb"><img src="/img/user.png" alt="">
+                        <!-- Social Info-->
+                        <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i
+                                    class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+                    </div>
+                    <!-- Team Details-->
+                    <div class="single_advisor_details_info">
+                        <h6>Bram Raiskay Chandra</h6>
+                        <p class="designation">215314049</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Advisor-->
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.3s"
+                    style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                    <!-- Team Thumb-->
+                    <div class="advisor_thumb"><img src="/img/user.png" alt="">
+                        <!-- Social Info-->
+                        <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i
+                                    class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+                    </div>
+                    <!-- Team Details-->
+                    <div class="single_advisor_details_info">
+                        <h6>Nathanael</h6>
+                        <p class="designation">nim</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Advisor-->
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.4s"
+                    style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                    <!-- Team Thumb-->
+                    <div class="advisor_thumb"><img src="/img/user.png" alt="">
+                        <!-- Social Info-->
+                        <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i
+                                    class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+                    </div>
+                    <!-- Team Details-->
+                    <div class="single_advisor_details_info">
+                        <h6>Noviana </h6>
+                        <p class="designation">nim</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Advisor-->
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.5s"
+                    style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                    <!-- Team Thumb-->
+                    <div class="advisor_thumb"><img src="/img/user.png" alt="">
+                        <!-- Social Info-->
+                        <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i
+                                    class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+                    </div>
+                    <!-- Team Details-->
+                    <div class="single_advisor_details_info">
+                        <h6>Faustina Fasa</h6>
+                        <p class="designation">nim</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\app\TicketingWeb\Ticketing\resources\views/about.blade.php ENDPATH**/ ?>
